d6cb9c98ed1a937badb7fc26cbad26c7307ad78b
stevenlegere <stevenmarklegere@gmail.com>
2023-09-29 22:05:21 +0100

    Added mock data in app with useState

--- notes-app/src/App.tsx
+++ notes-app/src/App.tsx
@@ -1,7 +1,47 @@
 import "./App.css";
+import {useState} from "react";
+
+// Define the Note type
+type Note = {
+  id: number;
+  title: string;
+  content: string;
+};
 
 // Create UI for the app
+
 const App = () => {
+
+// dummy data array for notes with id, title and content
+
+const [notes, setNotes] = useState<Note[]>([
+  {
+    id: 1,
+    title: "test note 1",
+    content: "hello note 1",
+  },
+  {
+    id: 2,
+    title: "test note 2",
+    content: "hello note 2",
+  },
+  {
+    id: 3,
+    title: "test note 3",
+    content: "hello note 3",
+  },
+  {
+    id: 4,
+    title: "test note 4",
+    content: "hello note 4",
+  },
+  {
+    id: 5,
+    title: "test note 5",
+    content: "hello note 5",
+  },
+]);
+
   return (
 <div className="AppContainer">
   <form className="note-form">
@@ -11,13 +51,15 @@ const App = () => {
     <button type="submit">Add Note</button>
   </form>
   <div className="notes-grid">
-    <div className="note-item">
+    {notes.map((note) => (
+    <div className="note-item" key={note.id}>
       <div className="notes-header">
         <button>x</button>
       </div>
       <h2>Note Title</h2>
       <p>Note Content</p>
     </div>
+    ))}
   </div>
 
 </div>
