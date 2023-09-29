5851cb904e756b992c03c79e064377d18dd7b0ac
stevenlegere <stevenmarklegere@gmail.com>
2023-09-29 22:22:06 +0100

    fixed state by including {} content in h2 and p

--- notes-app/src/App.tsx
+++ notes-app/src/App.tsx
@@ -56,8 +56,8 @@ const [notes, setNotes] = useState<Note[]>([
       <div className="notes-header">
         <button>x</button>
       </div>
-      <h2>Note Title</h2>
-      <p>Note Content</p>
+      <h2>{note.title}</h2>
+      <p>{note.content}</p>
     </div>
     ))}
   </div>
