bca50ab861073a97d184639b34550d2d38c342db
stevenlegere <stevenmarklegere@gmail.com>
2023-09-30 22:06:43 +0100

    connected prisma

--- notes-app/notes-app-server/index.ts
+++ notes-app/notes-app-server/index.ts
@@ -1,13 +1,16 @@
 import express from "express";
 import cors from "cors";
+import { PrismaClient } from "@prisma/client";
 
 const app = express(); // create express app
+const prisma = new PrismaClient(); // create prisma client
 
 app.use(express.json()); // use express json middleware (parses the json body of the request)
 app.use(cors()); // use cors middleware (allows cross origin requests)
 
-app.get("/api/motes", async (req, res) => {
-    res.json({ message: "success!" });
+app.get("/notes", async (req, res) => {
+    const notes = await prisma.note.findMany(); // get all notes from database
+    res.json(notes); // send notes as json response;
 });
 
 app.listen(5001, () => {
