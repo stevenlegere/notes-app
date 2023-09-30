58d1f160bb70594874963aaa9e3e219eec66e5a2
stevenlegere <stevenmarklegere@gmail.com>
2023-09-30 18:50:21 +0100

    created delete functionality

--- notes-app/src/App.tsx
+++ notes-app/src/App.tsx
@@ -105,6 +105,15 @@ const handleCancel = () => {
   setSelectedNote(null)
 };
 
+// Implement a function to delete a note
+const deleteNote = (event: React.MouseEvent, noteId: number) => {
+  event.stopPropagation();
+
+  const updatedNotes = notes.filter((note) => note.id !== noteId);
+
+  setNotes(updatedNotes);
+};
+
   return (
     <div className="AppContainer">
       <form className="note-form" 
@@ -136,7 +145,7 @@ const handleCancel = () => {
         {notes.map((note) => (
           <div className="note-item" key={note.id} onClick={() => handleNoteClick(note)}>
             <div className="notes-header">
-              <button>x</button>
+              <button onClick={(event) => deleteNote(event, note.id)}>x</button>
             </div>
             <h2>{note.title}</h2>
             <p>{note.content}</p>
